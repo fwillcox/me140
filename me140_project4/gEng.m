bf3da3c5cede43124e86a3b311607a9cf5a6be4f
Kendall <kendallf01@gmail.com>
2016-04-25 20:08:58 -0700

    removed for loop

--- me140_project4/gEng.m
+++ me140_project4/gEng.m
@@ -1,10 +1,22 @@
 %gEng.m , wrapper for energyF
 % 4-22-16 created by Jon Renslo
 function G = gEng(T,P,spec,mol)
-    
-    if(nargin == 3)     % Note: nargin returns number of function input arguments
-        mol = 1;
+    for i = 1:length(T)
+        if length(P) == 1
+            j = 1;
+        else
+            j = i;
+        end
+        if length(mol) == 1
+            k = 1;
+        else
+            k = i;
+        end
+        
+        if(nargin == 3)     % Note: nargin returns number of function input arguments
+            mol = 1;
+        end
+        a = energyF(T(i),P(j),spec,mol(k));
+        G(i) = a.G;
     end
-    a = energyF(T,P,spec,mol);
-    G = a.G;
 end
