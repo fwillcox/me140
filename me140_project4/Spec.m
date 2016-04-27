12632f471da76194ecba206ae6c8fa35545dfd43
Kendall <kendallf01@gmail.com>
2016-04-26 17:56:25 -0700

    finished part 3, added plot titles, still have full class return

---
+++ me140_project4/Spec.m
@@ -0,0 +1,21 @@
+classdef Spec
+    %SPEC Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        %mol_h2;
+        %mol_air;
+        mol_o2_react;
+        mol_n2;
+        %mol_o2_prod;
+        %mol_h2o;
+        %mol_total_react;
+        %mol_h2ovap;
+        %mol_h2oliq;
+    end
+    
+    methods
+    end
+    
+end
+
--- me140_project4/me140_project4.m
+++ me140_project4/me140_project4.m
@@ -39,15 +39,12 @@ lambda = 2;                             % Equivalence Ratio(ASSUME: 100% excess 
 Patm = 101.3*KPA_TO_PA;                 % Pa,     Preact = Pprod = Patm 
 % ------------------------------------------
 
-iterations =0;
 eta = zeros(size(T));
 pctVap = zeros(size(T));
 delG = zeros(size(T));
 for i = 1:length(T) %loop temperature (cols)
-    [eta(i), pctVap(i),delG(i)] = PEMstoich(lambda,T(i),Patm);
-    iterations = iterations + 1;
+    [eta(i), pctVap(i),delG(i),~] = PEMstoich(lambda,T(i),Patm);
 end
-iterations
 %PEMstoich assumes per mol of h2, 1mol h2 burned
 mass_h2 = 1*(MM_h*2)*G_TO_KG;
 eta_HHV = -delG / (HHV_h2 * mass_h2);
@@ -60,6 +57,7 @@ plot(T,eta_HHV,'b--', T,eta_LHV,'m--',T,eta,'g-', T,eta_carnot,'c');
 legend('\eta_{HHV}','\eta_{LHV}','\eta_{Mixed Liquid and Gas}','\eta_{Carnot}', 'Location', 'Best');
 xlabel('Temperature [K]');
 ylabel('Maximum 1st Law Efficiency');
+title('Part 1: First Law Efficiencies and Maximum Heat Engine Efficiency');
 plotfixer();
 
 % % UNCOMMENT FOR PART 2a (varying lambda)
@@ -70,7 +68,7 @@ Patm = 101.3*KPA_TO_PA;             % Pa,     Preact = Pprod = Patm
 
 for Ti = 1:length(T)
     for li = 1:length(lambda)
-        [etaLambda(li,Ti), pctVapLambda(li,Ti) ,delGLambda(li,Ti)] ...
+        [etaLambda(li,Ti), pctVapLambda(li,Ti) ,delGLambda(li,Ti),~] ...
             = PEMstoich(lambda(li),T(Ti),Patm);
     end
 end
@@ -82,6 +80,10 @@ plot(lambda,etaLambda_LHV);
 legend('80C','220C','650C','800C','Location','Best');
 xlabel('Excess air coefficient \lambda');
 ylabel('Efficiency on LHV basis \eta');
+title('Part 2: Varying Lambda: Maximum Cell Efficiency')
+plotfixer 
+spec = Spec();
+spec.mol_air = 5;
 
 %%part2.1 plot%%
 
@@ -104,7 +106,8 @@ plot(Patm/101300,etaPres_LHV);
 legend('80C','220C','650C','800C','Location','Best');
 xlabel('Pressure - Bar');
 ylabel('Efficiency on LHV basis \eta');
-return
+title('Part 2: Varying Pressure: Maximum Cell Efficiency')
+plotfixer
 
 % figure(2)
 % plot(T,mol_h2ovap);
@@ -122,57 +125,57 @@ return
 % must we take into account the diffusion thru membrane? -> don't need to
 % worry about gas diffusion through MEA membrane
 lambda = 2; % as before
+Patm = 101.3*KPA_TO_PA;             % Pa,     Preact = Pprod = Patm 
 Ptotal = Patm;
 % find psat at exit based on temp, 
 T = linspace(25,100,npts);
-psat = PsatW(T + C_TO_K);
+T = T + C_TO_K;
+psat = PsatW(T);
+
+%mol_h2 = zeros(size(T));
+%mol_air = zeros(size(T));
+mol_o2_react = zeros(size(T));
+mol_n2 = zeros(size(T));
+%mol_o2_prod = zeros(size(T));
+mol_h2o = zeros(size(T));
+mol_total_react = zeros(size(T));
+mol_h2ovap = zeros(size(T));
+mol_h2oliq = zeros(size(T));
+for i = 1:length(T)
+[~,~,~,tempSpecs] = PEMstoich(lambda, T(i), Ptotal);
+mol_h2(i) = tempSpecs.mol_h2;
+mol_air(i) = tempSpecs.mol_air ;
+mol_o2_react(i) = tempSpecs.mol_o2_react ;
+mol_n2(i) = tempSpecs.mol_n2 ;
+mol_o2_prod(i) = tempSpecs.mol_o2_prod ;
+mol_h2o(i) = tempSpecs.mol_h2o ;
+mol_total_react(i) = tempSpecs.mol_total_react ;
+mol_h2ovap(i) = tempSpecs.mol_h2ovap ;
+mol_h2oliq(i) = tempSpecs.mol_h2oliq ;
+end
+
+
 
 % find mole fraction of water in products
-y_h2o = psat./Ptotal;
-mol_out = (mol_o2_prod + mol_h2o + mol_n2);
-mol_h2o_sat = mol_out*y_h2o;
-
+y_h2o = psat./Ptotal; %Assume Pv = Psat
+beta = (4.26 .* y_h2o)./ (1 - y_h2o);
 % if less than what is formed, add the difference to dry air reagent
-alpha = mol_h2o_sat - mol_h2o;
-alpha(alpha<0) = 0;
-y_h2o_react = alpha./(mol_o2_react + mol_n2 + alpha);  
-Pv_react = Ptotal*y_h2o_react;
+alpha = beta - 1;
+%if condensation is forming just from what is formed, don't add any
+%humidity to reactants
+alpha(alpha < 0) = 0;
+y_h2o_react = alpha./(mol_o2_react + mol_n2 + alpha);
+Pv_react = Ptotal.*y_h2o_react;
 Pv_react(Pv_react>psat) = psat(Pv_react>psat); % if Pv > psat, Pv = psat 
 hum_rel = Pv_react./psat;
 
 % plot relative humidity
 % plot(T,alpha,T,omega2,T,hum_rel); - DELETE
 % legend('Moles of H2O to Add','Relative Humidity, outlet?') - DELETE
-figure(2);
-plot(T,hum_rel)
-legend('Relative Humidity of Input Air');
-xlabel('Temperature - K');
-ylabel('Relative Humidity %');
+figure(4);
+plot(T - C_TO_K,hum_rel)
+xlabel('Temperature [Celsius]');
+ylabel('Relative Humidity of Input Air [%]');
+title('Part 3: Relative Humidity as a Function of Temperature')
 plotfixer();
-
-
-
-% strategy for part 3:
-% calculate # mols of water in products at saturation (from mole fraction =
-% psat/p)--> mol_h2o_sat
-% then do atom balance - add water on reactant side - how many mols of
-% reactant water would u need to get result 
-% Pv/psat of products should be 1, then can use beta = alpha + 1 to relate
-% prods and reactants and find Pv of reactants w/ alpha, then just find
-% relative humidity = Pv/psat
-
-
-% % DELETE - FROM BEFORE
-% % find partial pressure of products
-% y_h2o_prod = mol_h2o/(mol_o2_prod + mol_h2o + mol_n2);
-% Pv = y_h2o_prod*Ptotal;
-% Pv(Pv>psat) = psat(Pv>psat); % if Pv > psat, Pv = psat 
-% figure()
-% plot(T, Pv./psat);
-% title('product rel hum - should be 1');
-
-%hum_rel = Pv./psat; 
-%hum_rel(Pv>psat) = 0; 
-%omega = Pv./(Ptotal-Pv)*(MM_h2o)/(MM_air); % formula from lecture does not seem to work.
-%omega2 = alpha*(MM_h2o)/(mol_o2_rxn*MM_o*2 + mol_n2*MM_n*2); 
 
