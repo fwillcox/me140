3b1ddb3d888d375663abfd273b81382745b48272
fwillcox <Frankie@DN0a22940c.SUNet>
2016-05-02 12:24:57 -0700

    fixed units for efficiencies
    still issues with eta_II

--- me140_project5/defineGlobals.m
+++ me140_project5/defineGlobals.m
@@ -1,14 +1,27 @@
 function  defineGlobals( )
 
-global PERMIN_TO_PERSEC PERHR_TO_PERSEC G_PER_KG LHV F N_TO_O SCF_TO_MOLS C_TO_K
+global PERMIN_TO_PERSEC PERHR_TO_PERSEC G_PER_KG LHV F N_TO_O SCF_TO_MOLS C_TO_K PSI_TO_PA
+% Temperature
 C_TO_K = 273.15;
+
+% Mass
 G_PER_KG = 1000;
-PERHR_TO_PERSEC = 60*60;
-PERMIN_TO_PERSEC = 60;
-LHV = 120.0*10^6;     % J/kg,  Lower Heating Value H2  
-F = 96485;         % C/(mol of e-), Faraday's Constant
-N_TO_O = 79/21;  
+
+% Time
+PERHR_TO_PERSEC = 1/(60*60);
+PERMIN_TO_PERSEC = 1/60;
+
+% Volume
 SCF_TO_MOLS = 1.19804;
+
+% Pressure
+PSI_TO_PA = 6894.76;
+
+% Useful Constants
+LHV = 120.0*10^6;     % J/kg,  Lower Heating Value H2  
+F = 96485;            % C/(mol of e-), Faraday's Constant
+N_TO_O = 79/21;  
+
 
 
 end
